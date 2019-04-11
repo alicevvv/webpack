0cb781debc796caeda0233588435e37ea32dc51e
alice <alicev871203@gmai.com>
2019-04-11 15:10:18 +0800

    image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+import $ from 'jquery';
+import bird from '../images/bird.jpg';
+import mouse from '../images/squirrel.jpg';
+
+$('#image_bird').attr("src",bird);
+$('#image_squirrel').attr("src",mouse);
+
+
 $(document).ready(function () {
 	// when a hanburger menu is clicked
 	let $ul = $(".nav__list"),
--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,30 @@ module.exports ={
 
     },
     module:{
-        rules:[{
-            test: /\.(js)$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader: 'babel-loader',
-                option:{
-                    presets:['@babel/preset-env']
+        rules:[
+            {
+                test: /\.(js)$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options:{
+                        presets:['@babel/preset-env']
+                    }
                 }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 40000,
+                            outputPath: './images'
+                        }
+                    },
+                    'image-webpack-loader'
+                ]
             }
-        }]
+        ]
     }
 }
